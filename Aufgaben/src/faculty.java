0501b9f4f7703a969f0605a4c173badde54d7222
churchmair <jonas1.kirchmair@gmail.com>
2022-11-10 11:09:29 +0100

    wrapper methode und eingabe in terminal

--- Aufgaben/src/faculty.java
+++ Aufgaben/src/faculty.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class faculty {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
 
-        int number = 6;
-        System.out.println("Das Ergebnis ist: " + facultyEndRecursive(1, number));
+        int number;
+        System.out.println("Bitte Zahl eingeben [0-16]");
+        number = reader.nextInt();
+
+        System.out.println("Das Ergebnis ist: " + faculty(number));
     }
+
+    private static int faculty(int zahl){
+        return facultyEndRecursive(1, zahl);
+    }
+
 
     public static int facultyEndRecursive(int sum, int fak){
         if (fak == 0){
