d3808b69cc9c35fd696e52a602771ff4f1853e5c
churchmair <jonas1.kirchmair@gmail.com>
2022-11-10 11:02:58 +0100

    variablen geändert

--- Aufgaben/src/faculty.java
+++ Aufgaben/src/faculty.java
@@ -3,21 +3,19 @@ public class faculty {
     public static void main(String[] args) {
 
         int number = 6;
-        System.out.println("Das Ergebnis ist: " + facultyRecursive(6));
+        System.out.println("Das Ergebnis ist: " + facultyEndRecursive(1, number));
     }
 
-    public static int facultyRecursive(int number){
-        return facultyEndRecursive(1, number);
-    }
-
-    public static int facultyEndRecursive(int m, int n){
-        if (n == 0){
-            return m;
+    public static int facultyEndRecursive(int sum, int fak){
+        if (fak == 0){
+            return sum;
         }
         else{
-             return facultyEndRecursive(m * n,n - 1);
+             return facultyEndRecursive(sum * fak,fak - 1);
         }
 
     }
+
+
 
 }
