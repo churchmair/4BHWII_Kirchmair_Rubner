7115d796a439a8de89aecca32428155ce8e1ac29
churchmair <jonas1.kirchmair@gmail.com>
2023-04-12 16:48:53 +0200

    aus 3 Factorys eine gemacht

--- Aufgaben/src/PizzaFactory/PizzaFactoryPattern.java
+++ Aufgaben/src/PizzaFactory/PizzaFactoryPattern.java
@@ -5,14 +5,12 @@ import PizzaFactory.Models.*;
 public class PizzaFactoryPattern {
     public static void main(String[] args) {
 
-        Pizzeria p1 = BerlinPizzeriaFactory.getBerlinPizzeria(Type.BerlinDiavolo, Preparation.backen);
-        Pizzeria p2 = BerlinPizzeriaFactory.getBerlinPizzeria(Type.BerlinSalami, Preparation.schneiden);
-
-        Pizzeria p3 = HamburgPizzeriaFactory.getHamburgPizzeria(Type.HamburgHawai, Preparation.einpacken);
-        Pizzeria p4 = HamburgPizzeriaFactory.getHamburgPizzeria(Type.HamburgSalami, Preparation.backen);
-
-        Pizzeria p5 = RostockPizzeriaFactory.getRostockPizzeria(Type.RostockMargharita, Preparation.schneiden);
-        Pizzeria p6 = RostockPizzeriaFactory.getRostockPizzeria(Type.RostockCalzone, Preparation.einpacken);
+        Pizzeria p1 = PizzeriaFactory.getPizzeria(Type.BerlinDiavolo, Preparation.backen);
+        Pizzeria p2 = PizzeriaFactory.getPizzeria(Type.BerlinSalami, Preparation.schneiden);
+        Pizzeria p3 = PizzeriaFactory.getPizzeria(Type.HamburgHawai, Preparation.einpacken);
+        Pizzeria p4 = PizzeriaFactory.getPizzeria(Type.HamburgSalami, Preparation.backen);
+        Pizzeria p5 = PizzeriaFactory.getPizzeria(Type.RostockMargharita, Preparation.schneiden);
+        Pizzeria p6 = PizzeriaFactory.getPizzeria(Type.RostockCalzone, Preparation.einpacken);
 
         System.out.println(p1);
         System.out.println(p2);
